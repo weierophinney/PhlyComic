8ca46d2a4055520f4b1a9ab09c642dbb388f7b7f
Matthew Weier O'Phinney <matthew@zend.com>
2013-08-28 13:26:25 -0500

    Allow fetching namespaced tag content
    - Added routine for fetching content tag; will fetch from namespace if
      a tag namespace is present.
    - Updated Sheldon source to define a namespace.

--- src/PhlyComic/ComicSource/AbstractRssSource.php
+++ src/PhlyComic/ComicSource/AbstractRssSource.php
@@ -23,6 +23,13 @@ abstract class AbstractRssSource extends AbstractComicSource
     protected $feedUrl;
 
     /**
+     * The namespace of the tag that holds the image, if any
+     * 
+     * @var false|string
+     */
+    protected $tagNamespace = false;
+
+    /**
      * What tag in the feed contains the image?
      * 
      * @var string
@@ -40,12 +47,13 @@ abstract class AbstractRssSource extends AbstractComicSource
         // daily is <link> element
         $daily = (string) $latest->link;
 
-        // image is in <description> -- /src="([^"]+)"
-        $desc  = (string) $latest->{$this->tagWithImage};
-        if (!preg_match('/src="(?P<src>[^"]+)"/', $desc, $matches)) {
+        $content  = $this->getContent($latest);
+
+        // image is in content -- /src="([^"]+)"
+        if (!preg_match('/src="(?P<src>[^"]+)"/', $content, $matches)) {
             return $this->registerError(sprintf(
                 static::$comics[$this->comicShortName] . ' feed does not include image description containing image URL: %s',
-                $desc
+                $content
             ));
         }
         $image = $matches['src'];
@@ -58,6 +66,16 @@ abstract class AbstractRssSource extends AbstractComicSource
         );
 
         return $comic;
+    }
+
+    protected function getContent(SimpleXMLElement $item)
+    {
+        if (!$this->tagNamespace) {
+            return (string) $latest->{$this->tagWithImage};
+        }
+
+        $namespacedChildren = $item->children($this->tagNamespace);
+        return (string) $namespacedChildren->{$this->tagWithImage};
     }
 
     protected function registerError($message)
