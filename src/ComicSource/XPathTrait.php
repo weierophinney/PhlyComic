852eb05e836f2d6ff154d70794715b9e174d92c1
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-03-11 08:31:39 -0600

    fix: do not call libxml_disable_entity_loader under PHP 8
    PHP 8 disables the functionality by default, and does not provide a way to enable it.
    The call itself raises a deprecation notice in PHP 8, so we need to suppress usage under that version.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- src/ComicSource/XPathTrait.php
+++ src/ComicSource/XPathTrait.php
@@ -11,13 +11,13 @@ trait XPathTrait
     protected function getXPathForDocument(string $content) : DOMXPath
     {
         libxml_use_internal_errors(true);
-        libxml_disable_entity_loader(true);
+        PHP_MAJOR_VERSION < 8 && libxml_disable_entity_loader(true);
 
         $document = new DOMDocument('1.0', 'UTF-8');
         $success = $document->loadHTML($content);
 
         $errors = libxml_get_errors();
-        libxml_disable_entity_loader(false);
+        PHP_MAJOR_VERSION < 8 && libxml_disable_entity_loader(false);
         libxml_use_internal_errors(false);
 
         if (! $success) {
