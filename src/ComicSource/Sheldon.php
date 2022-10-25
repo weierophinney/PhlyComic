e1553af225906bb2c8bd04746e555d643903e3bb
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-10-25 16:47:59 -0500

    fix: ensure we can pull new Sheldon comics
    The RSS feed has been broken for a while.
    Occasionally it will update correctly, and this utility will pull a new comic.
    However, it rarely stays fixed for more than a day.
    
    The new approach is:
    
    - Grab the home page
    - Grab the image for the most current comic from the home page
    - Traverse to the previous page
    - Grab the link to the most current comic via that previous page
    
    This works reliably for now (until the HTML changes).

--- src/ComicSource/Sheldon.php
+++ src/ComicSource/Sheldon.php
@@ -2,21 +2,92 @@
 
 namespace PhlyComic\ComicSource;
 
-class Sheldon extends AbstractRssSource
+use PhlyComic\Comic;
+use PhpCss;
+
+class Sheldon extends AbstractComicSource
 {
+    use XPathTrait;
+
+    private const BASE_URL = 'http://www.sheldoncomics.com/';
+    private const SELECTOR_NEXT = 'a#sidenav-next';
+    private const SELECTOR_PREV = 'a#sidenav-prev';
+    private const SELECTOR_COMIC = 'img#strip';
+
     protected static $comics = array(
         'sheldon' => 'Sheldon',
     );
 
-    protected $comicBase      = 'http://www.sheldoncomics.com';
-    protected $comicShortName = 'sheldon';
-    protected $feedUrl        = 'http://cdn.sheldoncomics.com/rss.xml';
-    protected $tagNamespace   = 'http://purl.org/rss/1.0/modules/content/';
-    protected $tagWithImage   = 'encoded';
-
-    protected function isOfInterest($item)
+    public function fetch()
     {
-        $link = (string) $item->guid;
-        return preg_match('#/archive/#', $link);
+        $page = file_get_contents(self::BASE_URL);
+        if ($page === false) {
+            return $this->registerError('Unable to reach Sheldon comic');
+        }
+
+        $imgUrl = $this->getUrlFromPageNode($page, self::SELECTOR_COMIC, 'src');
+        if ($imgUrl === null) {
+            return $this->registerError('Unable to locate Sheldon comic image');
+        }
+
+        $comicUrl = $this->getComicUrl($page);
+
+        return null === $comicUrl
+            ? new Comic(
+                static::$comics['sheldon'],
+                self::BASE_URL,
+                self::BASE_URL,
+                $imgUrl,
+            )
+            : new Comic(
+                static::$comics['sheldon'],
+                self::BASE_URL,
+                $comicUrl,
+                $imgUrl,
+            );
+    }
+
+    protected function registerError($message)
+    {
+        $comic = new Comic(
+            static::$comics['sheldon'],
+            self:: BASE_URL,
+        );
+        $comic->setError($message);
+        return $comic;
+    }
+
+    private function getUrlFromPageNode(string $content, string $selector, string $attribute = 'href'): ?string
+    {
+        $xpath = $this->getXPathForDocument($content);
+        $results = $xpath->query(PhpCss::toXpath($selector));
+        if (false === $results || 0 === count($results)) {
+            return null;
+        }
+
+        foreach ($results as $node) {
+            if (! $node->hasAttribute($attribute)) {
+                continue;
+            }
+
+            return $node->getAttribute($attribute);
+        }
+
+        return null;
+    }
+
+    private function getComicUrl(string $content): ?string
+    {
+        $previousUrl = $this->getUrlFromPageNode($content, self::SELECTOR_PREV);
+        if ($previousUrl === null) {
+            return null;
+        }
+
+        $page = file_get_contents($previousUrl);
+        if ($page === false) {
+            return null;
+        }
+
+        return $this->getUrlFromPageNode($page, self::SELECTOR_NEXT);
     }
 }
