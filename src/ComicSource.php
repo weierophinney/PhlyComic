f919285b43b92a468ee2bc222ff9b29ee7ebabff
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-04-04 15:42:24 -0500

    refactor: have ComicSource::fetch accept an http client
    This will allow instantiating them without needing a constructor, and they can use the provided HTTP client to do any fetching operations.

--- src/ComicSource.php
+++ src/ComicSource.php
@@ -2,11 +2,13 @@
 
 namespace PhlyComic;
 
+use Psr\Http\Client\ClientInterface;
+
 /**
  * Describes a comic source
  */
 interface ComicSource
 {
     public static function provides(): Comic;
-    public function fetch(): Comic;
+    public function fetch(ClientInterface $client): Comic;
 }
