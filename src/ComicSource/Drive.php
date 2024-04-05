2a17f39e3e6cc1b6e16368869e71d06553bf2406
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-04-05 09:14:19 -0500

    refactor: Drive

--- src/ComicSource/Drive.php
+++ src/ComicSource/Drive.php
@@ -2,19 +2,24 @@
 
 namespace PhlyComic\ComicSource;
 
+use PhlyComic\Comic;
+
 class Drive extends AbstractDomSource
 {
-    protected static $comics = array(
-        'drive' => 'drive',
-    );
-
-    protected $comicBase           = 'https://www.drivecomic.com';
-    protected $comicShortName      = 'drive';
     protected $domQuery            = 'div#unspliced-comic img';
     protected $domIsHtml           = true;
     protected $useComicBase        = true;
 
-    protected function validateImageSrc($src): bool
+    public static function provides(): Comic
+    {
+        return Comic::createBaseComic(
+            'drive',
+            'drive',
+            'https://www.drivecomic.com/',
+        );
+    }
+
+    protected function validateImageSrc(string $src): bool
     {
         return (bool) preg_match('#^https?://#', $src);
     }
