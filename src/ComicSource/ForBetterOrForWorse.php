990293deba570606624ac983b8d37a5cb093feaa
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-04-05 09:17:21 -0500

    refactor: For Better Or For Worse

--- src/ComicSource/ForBetterOrForWorse.php
+++ src/ComicSource/ForBetterOrForWorse.php
@@ -3,36 +3,33 @@
 namespace PhlyComic\ComicSource;
 
 use PhlyComic\Comic;
+use PhlyComic\HttpClient;
 
 class ForBetterOrForWorse extends AbstractComicSource
 {
-    protected static $comics = array(
-        'fborfw' => 'For Better or For Worse',
-    );
-
-    protected $comicBase = 'http://fborfw.com';
-
-    protected $dailyFormat = 'http://fborfw.com/strip_fix/%s/%s/%s/';
-    protected $imageFormat = 'http://fborfw.com/strip_fix/strips/fb%s.gif';
+    protected $dailyFormat       = 'http://fborfw.com/strip_fix/%s/%s/%s/';
+    protected $imageFormat       = 'http://fborfw.com/strip_fix/strips/fb%s.gif';
     protected $sundayImageFormat = 'http://fborfw.com/strip_fix/strips/fb%s.jpg';
 
-    public function fetch()
+    public static function provides(): Comic
     {
-        switch (strtolower(date('l'))) {
-            case 'sunday':
-                $format = $this->sundayImageFormat;
-                break;
-            default:
-                $format = $this->imageFormat;
-                break;
-        }
-
-        $comic = new Comic(
-            /* 'name'  => */ static::$comics['fborfw'],
-            /* 'link'  => */ $this->comicBase,
-            /* 'daily' => */ sprintf($this->dailyFormat, date('Y'), date('m'), date('d')),
-            /* 'image' => */ sprintf($format, date('ymd'))
+        return Comic::createBaseComic(
+            'fborfw',
+            'For Better or For Worse',
+            'http://fborfw.com/',
         );
-        return $comic;
+    }
+
+    public function fetch(HttpClient $client): Comic
+    {
+        $format = match(strtolower(date('l'))) {
+            'sunday' => $this->sundayImageFormat,
+            default  => $this->imageFormat,
+        };
+
+        return self::provides()->withInstance(
+            sprintf($this->dailyFormat, date('Y'), date('m'), date('d')),
+            sprintf($format, date('ymd')),
+        );
     }
 }
