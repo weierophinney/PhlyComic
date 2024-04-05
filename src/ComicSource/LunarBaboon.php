3fdc797c61250e977227d4be18481e558a41f3bd
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-04-05 09:44:23 -0500

    refactor: LunarBaboon

--- src/ComicSource/LunarBaboon.php
+++ src/ComicSource/LunarBaboon.php
@@ -2,13 +2,18 @@
 
 namespace PhlyComic\ComicSource;
 
+use PhlyComic\Comic;
+
 class LunarBaboon extends AbstractRssSource
 {
-    protected static $comics = array(
-        'lunarbaboon' => 'LunarBaboon',
-    );
+    protected $feedUrl = 'http://www.lunarbaboon.com/comics/rss.xml';
 
-    protected $comicBase      = 'http://www.lunarbaboon.com';
-    protected $comicShortName = 'lunarbaboon';
-    protected $feedUrl        = 'http://www.lunarbaboon.com/comics/rss.xml';
+    public static function provides(): Comic
+    {
+        return Comic::createBaseComic(
+            'lunarbaboon',
+            'LunarBaboon',
+            'http://www.lunarbaboon.com/',
+        );
+    }
 }
