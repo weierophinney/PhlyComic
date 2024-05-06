6fede048e4efcafb530043dbd1c950e81066663f
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-05-06 13:21:59 -0500

    fix: reverse feed order of NIH

--- src/ComicSource/NotInventedHere.php
+++ src/ComicSource/NotInventedHere.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace PhlyComic\ComicSource;
 
 use PhlyComic\Comic;
+use PhlyComic\HttpClient;
+use SimpleXMLElement;
+
+use function array_reverse;
+use function sprintf;
 
 class NotInventedHere extends AbstractAtomSource
 {
@@ -18,5 +23,50 @@ class NotInventedHere extends AbstractAtomSource
             'Not Invented Here',
             'https://notinventedhe.re',
         );
+    }
+
+    protected function getDataFromFeed(SimpleXMLElement $feed, HttpClient $client): Comic
+    {
+        foreach ($this->reverseEntries($feed) as $latest) {
+            if (! $this->isOfInterest($latest)) {
+                continue;
+            }
+
+            // daily is <link> element
+            $daily   = (string) $latest->id;
+            $content = $this->getContent($latest);
+            $image   = $this->getImageFromContent($content);
+
+            if ($image instanceof Comic) {
+                return $image;
+            }
+
+            if (! empty($image)) {
+                return static::provides()->withInstance($daily, $image);
+            }
+
+            // First item seeds content
+            if (null === $this->content) {
+                $this->content = $content;
+            }
+        }
+
+        $comic = static::provides();
+        return $comic->withError(sprintf(
+            '%s feed does not include image description containing image URL: %s',
+            $comic->name,
+            $this->content,
+        ));
+    }
+
+    /** @return iterable<SimpleXMLElement> */
+    private function reverseEntries(SimpleXMLElement $feed): iterable
+    {
+        $entries = [];
+        foreach ($feed->entry as $entry) {
+            $entries[] = $entry;
+        }
+
+        return array_reverse($entries);
     }
 }
