cc2aa36748bfacedfabe10d00634fa15fc6c603f
Matthew Weier O'Phinney <matthew@weierophinney.net>
2025-11-25 09:54:50 -0600

    fix: Lake Gary has a new feed
    - Feed URL changed
    - Feed structure changed slightly

--- src/ComicSource/LakeGary.php
+++ src/ComicSource/LakeGary.php
@@ -8,14 +8,17 @@ use PhlyComic\Comic;
 
 class LakeGary extends AbstractRssSource
 {
-    protected string $feedUrl = 'http://lakegary.com/rss';
+    protected string $feedUrl = 'https://lakegary.com/feed/';
+
+    protected string $tagWithImage = 'encoded';
+    protected false|string $tagNamespace = 'content';
 
     public static function provides(): Comic
     {
         return Comic::createBaseComic(
             'lakegary',
             'Lake Gary',
-            'http://lakegary.com/',
+            'https://lakegary.com/',
         );
     }
 }
